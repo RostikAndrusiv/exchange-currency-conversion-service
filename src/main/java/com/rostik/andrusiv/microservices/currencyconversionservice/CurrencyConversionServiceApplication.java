0e7441ad444e6ff36b790b1ad0272b4c833ce78c
RostikAndrusiv <rostik_andrusiv@ukr.net>
2022-04-12 20:51:19 +0300

    Initial commit

---
+++ src/main/java/com/rostik/andrusiv/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,16 @@
+package com.rostik.andrusiv.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableFeignClients("com.rostik.andrusiv.microservices.currencyconversionservice")
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
